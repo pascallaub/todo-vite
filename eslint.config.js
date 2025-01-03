9433629ea3a373e600ef65fb134480dbedff14f4
pascallaub <nutritionexpertition@gmail.com>
2025-01-03 16:52:37 +0100

    changes

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,20 @@ import globals from "globals";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
+  {
+    "env": {
+      "browser": true,
+      "es2021": true
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended", "prettier"],
+    "parserOptions": {
+      "ecmaVersion": 12,
+      "sourceType": "module"
+    },
+    "plugins": ["react", "prettier"],
+    "rules": {
+      "prettier/prettier": "error"
+    }
+  }  
 ];
